74d9feff9eedf090a9fd5aef836f313787d8d652
Md. Sabbir Ahmed <85029908+AbuNayemSabbir@users.noreply.github.com>
2022-08-17 10:52:42 +0600

    init

---
+++ app/src/main/java/com/example/fragmentexample/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.fragmentexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager().beginTransaction().add(R.id.fragmentContainerView,new OneFragment()).commit();
+
+
+    }
+}
